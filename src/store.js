d64a4a84e3cefb773ddd0153c218394c263d9853
Reuven yayech <rovenye@gmail.com>
2018-12-26 21:47:23 +0200

    add first commit

--- src/store.js
+++ src/store.js
@@ -1,17 +1,59 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import { stat } from 'fs';
+const fs = require('fs');
+
+
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    
-    
+    SheetIds: []
   },
+  getters:{
+    getSheetIds(state){
+        return state.sheetIds;
+    }
+  },  
   mutations: {
-
-  },
+    loadData(state){
+      try { 
+         var sheetIds =  fs.readFileSync('SheetIds.json','utf-8')
+         const sheetIdsJSON = JSON.parse(sheetIds)
+         state.sheetIds.push(sheetIdsJSON)
+         }
+        catch(e) {
+           alert('Failed to save the file !');
+       }
+    },
+    saveFile (state,payload) {
+      try { 
+      var sheetIds =  fs.readFileSync('SheetIds.json','utf-8')
+      
+        const sheetIdsJSON = JSON.parse(sheetIds)
+        
+        sheetIdsJSON.sheets.push({
+          "id":payload.id,
+          "name":payload.name,
+          "version":payload.version,
+          "is_valid":payload.isValid,
+          "date":payload.date
+        });
+        fs.writeFileSync('SheetIds.json', JSON.stringify(sheetIdsJSON));
+        state.sheetIds = sheetIdsJSON;
+       }
+      catch(e) {
+         alert('Failed to save the file !');
+     }
+  }
+},
   actions: {
-
+    loadData ({ commit, dispatch }) {
+      commit('loadData', dispatch );
+    },
+    saveFile ({commit},payload) {
+      commit('saveFile',payload);
+    }
   }
 })
--- src/views/Setting.vue
+++ src/views/Setting.vue
@@ -1,5 +1,53 @@
 <template>
   <div class="setting">
-    <h1>This is an setting page</h1>
+    <h1>Set Sheet Id</h1>
+    <input v-model="sheetid" placeholder="edit me">
+    <button class="btn-sheet-id" @click="SetSheetId">Click me !</button>
+     <ul class="message-list" ref="list">
+     <li v-for="sheetId in SheetIds">
+    {{ sheetId.id }} - {{ sheetId.name }} - {{ sheetId.isValid }}
+  </li>
+    </ul>
   </div>
 </template>
+<script>
+export default {
+  name: "setting",
+  computed: {
+    SheetIds() {
+      return this.$store.getters.getSheetIds;
+    }
+  },
+  data: () => {
+    return {
+      sheetid: ""
+    };
+  },
+  methods:{ 
+    SetSheetId(){
+      var sheet = {
+                   id:this.sheetid,
+                   name:"V-"+this.sheetid,
+                   version:"1.1.95",
+                   isValid:false,
+                   date:"22/03/2016 14:03:01"
+                   }
+
+      this.$store.dispatch('saveFile',sheet)
+    }
+  }
+  
+};
+</script>
+
+<style>
+.message-list {
+  border: 1px solid #ccf;
+  font-size: 16px;
+  height: 400px;
+  margin: 0;
+  overflow-y: auto;
+  padding: 0;
+}
+</style>
+
